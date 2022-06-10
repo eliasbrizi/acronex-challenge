715b6b75d55bc9213354931d2f6bd4b97a0e4225
Elias <eliasbrizi@yahoo.es>
2022-06-10 15:46:04 -0300

    done update machine

--- backend/machines/urls.py
+++ backend/machines/urls.py
@@ -3,5 +3,5 @@ from machines import views
 
 urlpatterns = [
     path('api/machines', views.machine_list),
-    # path(r'^api/machines/(?P<pk>[0-9]+)$', views.machine_detail),
+    path(r'api/machines/<int:id>', views.machine_detail),
 ]
