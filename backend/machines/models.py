4938b8cd70d6d33e7f022fbaff2041c40674ad06
Elias <eliasbrizi@yahoo.es>
2022-06-10 11:29:16 -0300

    created machines models

--- backend/machines/models.py
+++ backend/machines/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class MachineType(models.Model):
+    name = models.CharField(max_length=50, blank=False, default='')
+
+
+class Machine(models.Model):
+    description = models.CharField(max_length=200, blank=False, default='')
+    company = models.CharField(max_length=60, blank=False, default='')
+    machineType = models.ForeignKey(MachineType, on_delete=models.CASCADE)
